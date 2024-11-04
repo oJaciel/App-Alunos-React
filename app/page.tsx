080bed7429acd305ee44428df342781277fa05b9
Jaciel <jacielhbarichello@gmail.com>
2024-11-04 20:48:54 -0300

    Noções Iniciais React
    Desenvolvidos alguns componentes e funções para entendimento do React.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,43 @@
-import Image from "next/image";
+'use client';
+
+import { useState } from "react";
 
 export default function Home() {
   return (
-     <h1>Bem vindo</h1>
-     
-     );
+    <div>
+      <h1>Bem vindo</h1>
+      <p>Bem vindo ao React</p>
+      <CapsLockComponent texto="transforme o texto" />
+
+      <br/>
+      <br/>
+
+      <Contador />
+
+    </div>
+  );
+}
+
+
+export function CapsLockComponent(props: {texto:string}) {
+  let capsText = props.texto.toUpperCase();
+  return <p>{capsText}</p>
+}
+
+
+export function Contador() {
+
+  const [num, setNum] = useState(1)
+
+  function incrementar() {
+    setNum(num + 1)
+    console.log(num)
+  }
+
+  return (
+    <div>
+      <h3>{num}</h3>
+      <button onClick={incrementar}>Incrementar</button>
+    </div>
+  );
 }
